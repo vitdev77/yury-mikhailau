7401454a9542dc96b89f4e6b96da6c4e0b4929d6
vitdev77 <vit.dev.77@gmail.com>
2024-02-06 20:10:45 +0100

    first commit

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,59 @@
+import "@/styles/globals.css";
+import type { Metadata, Viewport } from "next";
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/lib/fonts";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/app/[locale]/components/theme-provider";
+import { SiteHeader } from "@/app/[locale]/components/site-header";
+// import { SiteFooter } from "@/app/[locale]/components/site-footer";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+  params: { locale: string };
+}
+
+export default function RootLayout({
+  children,
+  params: { locale },
+}: RootLayoutProps) {
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.className
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="relative flex min-h-screen flex-col bg-background">
+            <SiteHeader locale={locale} />
+            <main className="flex-1">{children}</main>
+            {/* <SiteFooter /> */}
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "My CV",
+  url: "http://localhost:3000",
+  ogImage: "",
+  description: "",
+  links: {
+    twitter: "",
+    github: "",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
